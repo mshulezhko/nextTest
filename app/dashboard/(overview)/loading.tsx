ff1dd05c7b81e3e37618c0812d56d16a511f4493
mshulezhko <119418285+mshulezhko@users.noreply.github.com>
2024-02-18 13:27:27 +0100

    chapter 9

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import DashboardSkeleton from '@/app/ui/skeletons';
+
+const Loading = () => {
+  return (
+    <>
+      ............++++
+      <DashboardSkeleton />
+    </>
+  );
+};
+
+export default Loading;
